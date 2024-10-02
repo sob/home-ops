6d08424873e667845eb207590a002242c4c76334
Sean O'Brien <sean@obrienfamily.org>
2024-10-02 18:55:48 -0500

    [repo] update task files

--- .taskfiles/volsync/resources/wait-for-job.sh
+++ .taskfiles/volsync/resources/wait-for-job.sh
@@ -5,7 +5,7 @@ NAMESPACE="${2:-default}"
 
 [[ -z "${JOB}" ]] && echo "Job name not specified" && exit 1
 while true; do
-    STATUS="$(kubectl -n "${NAMESPACE}" get pod -l job-name="${JOB}" -o jsonpath='{.items[*].status.phase}')"
+    STATUS="$(kubectl --namespace "${NAMESPACE}" get pod --selector="job-name=${JOB}" --output="jsonpath='{.items[*].status.phase}'")"
     if [ "${STATUS}" == "Pending" ]; then
         break
     fi
