4a49950a39171937799283a851c3885fe30f5fce
Sean O'Brien <sean@obrienfamily.org>
2025-01-01 20:12:38 -0600

    [authentik] fix terraform directory

--- terraform/authentik/directory.tf
+++ terraform/authentik/directory.tf
@@ -8,30 +8,19 @@ resource "authentik_group" "media" {
   is_superuser = false
 }
 
-resource "authentik_group" "grafana_admin" {
-  name         = "Grafana Admins"
-  is_superuser = false
-}
-
-resource "authentik_group" "headscale" {
-  name         = "Headscale"
-  is_superuser = false
-}
-
 resource "authentik_group" "home" {
-  name         = "Home"
+  name         = "home"
   is_superuser = false
 }
 
-resource "authentik_group" "infrastructure" {
-  name         = "Infrastructure"
+resource "authentik_group" "observability" {
+  name         = "observability"
   is_superuser = false
 }
 
-resource "authentik_group" "monitoring" {
-  name         = "Monitoring"
+resource "authentik_group" "network" {
+  name         = "network"
   is_superuser = false
-  parent       = resource.authentik_group.grafana_admin.id
 }
 
 resource "authentik_group" "users" {
@@ -46,7 +35,10 @@ resource "authentik_user" "sob" {
   password = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_SOB_PASSWORD
   groups = [
     data.authentik_group.admins.id,
-    data.authentik_group.media.id,
+    resource.authentik_group.home.id,
+    resource.authentik_group.media.id,
+    resource.authentik_group.network.id,
+    resource.authentik_group.observability.id
   ]
 }
 
@@ -54,22 +46,35 @@ resource "authentik_user" "lob" {
   username = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_LOB_USERNAME
   name     = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_LOB_NAME
   email    = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_LOB_EMAIL
+  groups = [
+    resource.authentik_group.home.id,
+    resource.authentik_group.media.id
+  ]
 }
 
 resource "authentik_user" "gob" {
   username = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_GOB_USERNAME
   name     = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_GOB_NAME
   email    = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_GOB_EMAIL
+  groups = [
+    resource.authentik_group.media.id
+  ]
 }
 
 resource "authentik_user" "eob" {
   username = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_EOB_USERNAME
   name     = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_EOB_NAME
   email    = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_EOB_EMAIL
+  groups = [
+    resource.authentik_group.media.id
+  ]
 }
 
 resource "authentik_user" "cob" {
   username = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_COB_USERNAME
   name     = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_COB_NAME
   email    = module.onepassword_authentik.fields.AUTHENTIK_BLUEPRINTS_USERS_COB_EMAIL
+  groups = [
+    resource.authentik_group.media.id
+  ]
 }
