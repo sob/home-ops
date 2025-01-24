0c0870d5f8dac97d58878edb18ac83efafa9cab2
Sean O'Brien <sean@obrienfamily.org>
2025-01-23 21:36:10 -0600

    [terraform] add media to prvision radarr, sonarr, and prowlarr

---
+++ terraform/media/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    prowlarr = {
+      source = "devopsarr/prowlarr"
+      version = "2.4.3"
+    }
+    radarr = {
+      source = "devopsarr/radarr"
+      version = "2.3.1"
+    }
+    sonarr = {
+      source = "devopsarr/sonarr"
+      version = "3.4.0"
+    }
+    lidarr = {
+      source = "devopsarr/lidarr"
+      version = "1.13.0"
+    }
+    onepassword = {
+      source = "1password/onepassword"
+    }
+  }
+}
+
+provider "onepassword" {
+  account = var.onepassword_account
+}
+
+module "onepassword_prowlarr" {
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "STONEHEDGES"
+  item   = "prowlarr"
+}
+
+module "onepassword_sonarr" {
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "STONEHEDGES"
+  item   = "sonarr"
+}
+
+module "onepassword_radarr" {
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "STONEHEDGES"
+  item   = "radarr"
+}
+
+module "onepassword_readarr" {
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "STONEHEDGES"
+  item   = "readarr"
+}
+
+module "onepassword_lidarr" {
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "STONEHEDGES"
+  item   = "lidarr"
+}
+
+module "onepassword_sabnzbd" {
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "STONEHEDGES"
+  item   = "sabnzbd"
+}
---
+++ terraform/media/sonarr.tf
@@ -0,0 +1,73 @@
+provider "sonarr" {
+  url     = module.onepassword_sonarr.fields.SONARR_URL
+  api_key = module.onepassword_sonarr.fields.SONARR_API_KEY
+}
+
+resource "sonarr_root_folder" "series" {
+  path = "/media/Library/series"
+}
+
+resource "sonarr_root_folder" "series-uhd" {
+  path = "/media/Library/series-uhd"
+}
+
+resource "sonarr_download_client_sabnzbd" "sabnzbd" {
+  enable                     = true
+  priority                   = 1
+  name                       = "SABnzbd"
+  host                       = "sabnzbd.default.svc.cluster.local"
+  port                       = "80"
+  api_key                    = module.onepassword_sabnzbd.fields.SABNZBD_API_KEY
+  tv_category                = "tv"
+  use_ssl                    = false
+  remove_completed_downloads = true
+  remove_failed_downloads    = true
+}
+
+resource "sonarr_remote_path_mapping" "sabnzbd" {
+  host        = "sabnzbd.default.svc.cluster.local"
+  remote_path = "/Downloads/sabnzbd/complete/"
+  local_path  = "/media/Downloads/sabnzbd/complete/"
+}
+
+resource "sonarr_host" "sonarr" {
+  depends_on = [
+    sonarr_root_folder.series,
+    sonarr_root_folder.series-uhd,
+    sonarr_download_client_sabnzbd.sabnzbd,
+    sonarr_remote_path_mapping.sabnzbd
+  ]
+  launch_browser = true
+  port = 80
+  url_base = ""
+  bind_address = "*"
+  application_url = ""
+  instance_name = "Sonarr"
+
+  authentication = {
+    method = "external"
+  }
+  proxy = {
+    enabled = false
+    bypass_local_addresses = true
+  }
+  ssl = {
+    enabled = false
+    certificate_validation = "enabled"
+  }
+  logging = {
+    log_level = "debug"
+    analytics_enabled = false
+    log_size_limit = 1
+  }
+  backup = {
+    folder = "Backups"
+    interval = 7
+    retention = 28
+  }
+  update = {
+    mechanism = "docker"
+    branch = "develop"
+    update_automatically = false
+  }
+}
